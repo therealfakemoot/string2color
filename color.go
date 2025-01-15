f86f2c795d09e03b61b90b0be07eee96318f03a8
Nick Dumas <nobodys@business.com>
2025-01-14 19:44:16 -0500

    Switch over to using image/color.Color

--- color.go
+++ color.go
@@ -3,58 +3,41 @@ package string2color
 import (
 	"crypto/sha1"
 	"encoding/binary"
-	"fmt"
+	"image/color"
 	"io"
 	"math"
 
 	"golang.org/x/exp/constraints"
 )
 
-type RGB struct {
-	R, G, B uint64
-}
-
-func (rgb RGB) String() string {
-	return fmt.Sprintf("{R:%x,G:%x,B:%x}", rgb.R, rgb.G, rgb.B)
-}
-
-type RGBA struct {
-	RGB
-	A uint64
-}
-
-func (rgba RGBA) String() string {
-	return fmt.Sprintf("{R:%x,G:%x,B:%x,A:%x}", rgba.R, rgba.G, rgba.B, rgba.A)
-}
-
-func ToRGB(s string) RGB {
+func ToRGB(s string) color.RGBA {
 	h := sha1.New()
 	io.WriteString(h, s)
 	sum := h.Sum(nil)
-	var rgb RGB
+	var rgb color.RGBA
 	r := binary.LittleEndian.Uint64(sum[:9])
 	g := binary.LittleEndian.Uint64(sum[7:16])
 	b := binary.LittleEndian.Uint64(sum[11:])
-	rgb.R = InterpolateUint64(r, 0, uint64(math.Pow(2, 56)), 0, 64384)
-	rgb.G = InterpolateUint64(g, 0, uint64(math.Pow(2, 56)), 0, 64384)
-	rgb.B = InterpolateUint64(b, 0, uint64(math.Pow(2, 56)), 0, 64384)
+	rgb.R = uint8(InterpolateUint64(r, 0, uint64(math.Pow(2, 56)), 0, 64384))
+	rgb.G = uint8(InterpolateUint64(g, 0, uint64(math.Pow(2, 56)), 0, 64384))
+	rgb.B = uint8(InterpolateUint64(b, 0, uint64(math.Pow(2, 56)), 0, 64384))
 
 	return rgb
 }
 
-func ToRGBA(s string) RGBA {
+func ToRGBA(s string) color.RGBA {
 	h := sha1.New()
 	io.WriteString(h, s)
 	sum := h.Sum(nil)
-	var rgba RGBA
+	var rgba color.RGBA
 	r := binary.LittleEndian.Uint64(sum[:9])
 	g := binary.LittleEndian.Uint64(sum[5:14])
 	b := binary.LittleEndian.Uint64(sum[9:18])
 	a := binary.LittleEndian.Uint64(sum[12:])
-	rgba.R = InterpolateUint64(r, 0, uint64(math.Pow(2, 56)), 0, 64384)
-	rgba.G = InterpolateUint64(g, 0, uint64(math.Pow(2, 56)), 0, 64384)
-	rgba.B = InterpolateUint64(b, 0, uint64(math.Pow(2, 56)), 0, 64384)
-	rgba.A = InterpolateUint64(a, 0, uint64(math.Pow(2, 56)), 0, 64384)
+	rgba.R = uint8(InterpolateUint64(r, 0, uint64(math.Pow(2, 56)), 0, 64384))
+	rgba.G = uint8(InterpolateUint64(g, 0, uint64(math.Pow(2, 56)), 0, 64384))
+	rgba.B = uint8(InterpolateUint64(b, 0, uint64(math.Pow(2, 56)), 0, 64384))
+	rgba.A = uint8(InterpolateUint64(a, 0, uint64(math.Pow(2, 56)), 0, 64384))
 
 	return rgba
 }
